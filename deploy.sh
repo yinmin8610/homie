63e67c5702ef4f70bb65630a7caf0aaaba4a3e5b
yinmin <alice0050722@gmail.com>
2020-03-25 02:43:56 +0800

    推推gh-pg

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,11 @@ npm run build
 cd dist
 # 部署到自定义域域名
 # echo 'www.example.com' > CNAME
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+git init
 git add -A
 git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 # 部署到 https://github.com/yinmin8610/homie.git 分支為 gh-pages
 git push -f https://github.com/yinmin8610/homie.git master:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
